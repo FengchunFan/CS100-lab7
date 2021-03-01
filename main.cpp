f2aa1abb76f4b3df932e76c9440c64db29a93225
github-username <ffan005@ucr.edu>
2021-02-28 16:06:53 -0800

    modified dmain.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "factory.hpp"
+
+int main(int argv, char** argc){
+
+Factory* factory = new Factory();
+Base* calculate = factory -> parse(argc, argv);
+cout << calculate -> stringify() << endl << calculate -> evaluate() << endl;
+
+return 0;
+}
